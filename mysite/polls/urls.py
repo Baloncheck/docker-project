2f2fe116c7da984267be9748fc06c7969ce92018
baloncek2662 <martic.gasper@gmail.com>
2018-07-03 13:33:44 +0200

    create and edit new views

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -2,6 +2,18 @@ from django.conf.urls import url
 
 from . import views
 
+from django.views.generic import RedirectView
+
+
+app_name = 'polls'
 urlpatterns = [
+    #url(r'/', RedirectView.as_view(url='/polls')), try to redirect from / to /polls
+    # ex: /polls/
     url(r'^$', views.index, name='index'),
+    # ex: /polls/5/
+    url(r'^(?P<question_id>[0-9]+)/$', views.detail, name='detail'),
+    # ex: /polls/5/results/
+    url(r'^(?P<question_id>[0-9]+)/results/$', views.results, name='results'),
+    # ex: /polls/5/vote/
+    url(r'^(?P<question_id>[0-9]+)/vote/$', views.vote, name='vote'),
 ]
